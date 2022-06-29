8452813e4adb2f790a2f29d51def676d81929c84
Mikhail Vyrostkov <m.vyrostkov@tcsbank.ru>
2022-06-29 17:43:41 +0300

    first commit

---
+++ src/test/java/vyrostkov/springframework/sfpetclinic/SfPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package vyrostkov.springframework.sfpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
