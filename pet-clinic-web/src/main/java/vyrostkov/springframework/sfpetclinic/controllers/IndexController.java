64da3c6548506750c2a74c1fbb2f6374c9d2ca75
m.vyrostkov <mikhail-vyrostkov@mail.ru>
2022-09-22 18:09:06 +0300

    Fixed broken links

--- pet-clinic-web/src/main/java/vyrostkov/springframework/sfpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/vyrostkov/springframework/sfpetclinic/controllers/IndexController.java
@@ -11,7 +11,11 @@ public class IndexController {
 
     @RequestMapping({"", "/", "index", "index.html"})
     public String index(){
-
         return "index";
+    }
+
+    @RequestMapping("/oups")
+    public String oupsHandler(){
+        return "notimplemented";
     }
 }
--- pet-clinic-web/src/main/java/vyrostkov/springframework/sfpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/vyrostkov/springframework/sfpetclinic/controllers/OwnerController.java
@@ -24,4 +24,9 @@ public class OwnerController {
         model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
+
+    @RequestMapping("/find")
+    public String findOwners(){
+        return "notimplemented";
+    }
 }
