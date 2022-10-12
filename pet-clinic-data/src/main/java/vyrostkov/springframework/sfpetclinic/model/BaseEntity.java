54e724a9dcef683de5fe8ae34403595ea5e8a832
m.vyrostkov <mikhail-vyrostkov@mail.ru>
2022-10-12 15:11:14 +0300

    Updated base entity with JPA annotations

--- pet-clinic-data/src/main/java/vyrostkov/springframework/sfpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/vyrostkov/springframework/sfpetclinic/model/BaseEntity.java
@@ -1,12 +1,18 @@
 package vyrostkov.springframework.sfpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
  * Created by @m.vyrostkov on 07.07.2022 16:44
  */
-public class BaseEntity  implements Serializable {
-
+@MappedSuperclass
+public class BaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
