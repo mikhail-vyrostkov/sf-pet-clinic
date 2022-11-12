32d0f4838dbf9817692ca2110a7bca2a019065ac
m.vyrostkov <mikhail-vyrostkov@mail.ru>
2022-11-12 18:54:12 +0300

    Implemented Map impl for Visit Service

---
+++ pet-clinic-data/src/main/java/vyrostkov/springframework/sfpetclinic/services/VisitService.java
@@ -0,0 +1,9 @@
+package vyrostkov.springframework.sfpetclinic.services;
+
+import vyrostkov.springframework.sfpetclinic.model.Visit;
+
+/**
+ * Created by @m.vyrostkov on 12.11.2022 18:29
+ */
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/vyrostkov/springframework/sfpetclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,45 @@
+package vyrostkov.springframework.sfpetclinic.services.map;
+
+import org.springframework.stereotype.Service;
+import vyrostkov.springframework.sfpetclinic.model.Owner;
+import vyrostkov.springframework.sfpetclinic.model.Visit;
+import vyrostkov.springframework.sfpetclinic.services.OwnerService;
+import vyrostkov.springframework.sfpetclinic.services.VisitService;
+
+import java.util.Set;
+
+/**
+ * Created by @m.vyrostkov on 12.11.2022 18:35
+ */
+@Service
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        if (object.getPet() == null || object.getPet().getOwner() == null || object.getPet().getId() == null
+                || object.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(object);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
