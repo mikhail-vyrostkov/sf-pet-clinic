644c42c6758b80826aa519f373fda6f5d4e25a25
m.vyrostkov <mikhail-vyrostkov@mail.ru>
2022-11-21 19:02:24 +0300

    upgraded to JUnit 5

--- pet-clinic-web/src/test/java/vyrostkov/springframework/sfpetclinic/SfPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/vyrostkov/springframework/sfpetclinic/SfPetClinicApplicationTests.java
@@ -1,8 +1,12 @@
 package vyrostkov.springframework.sfpetclinic;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class SfPetClinicApplicationTests {
 
